903bae50ec25afabdef3ad20636768fb18558583
leopereirasurf <digitarr123@gmail.com>
2023-03-03 17:41:10 -0300

    php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Primeiroprograma em PHP</title>
+</head>
+<body>
+    <h1>
+<?php 
+echo 'Olá mundo!...'
+?>
+<p>Vanmos tenter se livar da Maldição</p>
+    </h1>
+</body>
+</html>
